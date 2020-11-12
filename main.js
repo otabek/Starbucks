e72e37c4afe13470d5419ae554223e275ee28ea8
Otabek <otabekn20@gmail.com>
2020-11-12 15:10:30 +0500

    new

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(slide) {
+    document.querySelector('.starbucks').src = slide;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
